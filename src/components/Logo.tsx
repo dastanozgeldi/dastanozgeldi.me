d01c36590e92c4d9da72c2a101b5373805ef1f61
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-28 13:05:44 +0600

    feat(components): add navbar

---
+++ src/components/Logo.tsx
@@ -0,0 +1,14 @@
+import Image from "next/image";
+import Link from "next/link";
+import hoof from "../assets/hoof.png";
+
+export default function Logo() {
+  return (
+    <Link href="/">
+      <div className="flex items-center gap-2">
+        <Image src={hoof} width={20} height={20} alt="Logo Icon" />
+        Dastan Ozgeldi
+      </div>
+    </Link>
+  );
+}
