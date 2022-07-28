9f2e95d15eade473d1ee9f9a45fdf6a94ed35c22
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-28 14:48:43 +0600

    feat(components): logo cursor-pointer

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -5,7 +5,7 @@ import hoof from "../assets/hoof.png";
 export default function Logo() {
   return (
     <Link href="/">
-      <div className="flex items-center gap-2">
+      <div className="flex items-center gap-2 cursor-pointer">
         <Image src={hoof} width={20} height={20} alt="Logo Icon" />
         Dastan Ozgeldi
       </div>
