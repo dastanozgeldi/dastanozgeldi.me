0538f7275267f14eec5666b047e90d74c4204819
Dastan Ozgeldi <ozgdastan@gmail.com>
2022-07-28 13:04:11 +0600

    feat(root): add colors to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#0295DB",
+        secondary: "#FFD000",
+        bg: "#202020",
+      },
+    },
   },
   plugins: [],
 };
