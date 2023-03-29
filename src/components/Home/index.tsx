6d0686a8452b606535c7328c9acfddd70667e242
Dastan Ozgeldi <ozgdastan@gmail.com>
2023-03-29 21:42:48 +0600

    fix: renaming folder part 1

--- src/components/Home/index.tsx
+++
@@ -1,6 +0,0 @@
-import { About } from "./About";
-import { Bio } from "./Bio";
-import { LastEvent } from "./LastEvent";
-import { Profile } from "./Profile";
-
-export { About, Bio, LastEvent, Profile };
