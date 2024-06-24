a9a4cc725b40602e95260070b9c35ea84517d4ed
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-06-24 22:03:59 +0500

    feat: remove command menu from nav

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,20 +3,19 @@
 import Link from "next/link";
 
 import { ThemeToggle } from "./theme-toggle";
-import { CommandMenu } from "./command-menu";
 
 export const Header = () => {
   return (
     <header className="mb-5 md:mb-10 flex items-center justify-between">
       <span className="text-md md:text-lg whitespace-nowrap font-bold">
         <Link href="/">
-          <span className="font-serif cursor-pointer pr-2">Dastan Özgeldi</span>
+          <span className="font-serif cursor-pointer pr-2">Dastan Ozgeldi</span>
         </Link>
       </span>
 
       <div className="flex flex-row ml-0 sm:ml-2 space-x-2 items-center justify-center sm:justify-end">
+        <Link href="/blog">blog</Link>
         <ThemeToggle />
-        <CommandMenu />
       </div>
     </header>
   );
