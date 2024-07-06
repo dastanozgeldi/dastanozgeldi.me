30657a49acf73a9d2f2fbc766dabf1b576ac057b
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-07-06 11:41:47 +0500

    update age in website config

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,7 @@
 export const site = {
   url: "https://dastanozgeldi.me/",
-  title: "Dastan Özgeldi",
+  title: "Dastan Ozgeldi",
   keywords: ["dastanozgeldi", "Next.js", "React", "TypeScript", "Node.js"],
-  description: "Dastan • 16 y/o • Student • Full-stack Developer",
+  description: "Dastan • 17 y/o • Student • Full-stack Developer",
   image: "https://dastanozgeldi.me/brand.png",
 };
