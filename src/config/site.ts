44bf83264fa85484bed0e3e69c3d5fc94b5bf2a8
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-11-02 17:24:02 +0500

    feat: add a couple more keywords for seo

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,7 +1,15 @@
 export const site = {
   url: "https://dastanozgeldi.me/",
   title: "Dastan Ozgeldi",
-  keywords: ["dastanozgeldi", "Next.js", "React", "TypeScript", "Node.js"],
+  keywords: [
+    "Дастан Өзгелді",
+    "Дастан Озгелди",
+    "dastanozgeldi",
+    "Next.js",
+    "React",
+    "TypeScript",
+    "Node.js",
+  ],
   description: "Dastan • 17 y/o • Student • Full-stack Developer",
   image: "https://dastanozgeldi.me/brand.png",
 };
