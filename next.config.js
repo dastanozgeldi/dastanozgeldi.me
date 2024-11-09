616a27821b498fe13552d0cf3e558da4ad80d32b
Dastan Ozgeldi <ozgdastan@gmail.com>
2024-11-10 01:17:44 +0500

    feat: prepare next config to use uploadthing as a cdn

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/a/u0e5kx6sa4/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
