299cd656f65ea6cf1d35d09a566c016c8a6e9caf
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-15 02:02:35 +0500

    add profile picture to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,35 @@
 import Link from "next/link";
 import { ProjectList } from "@/components/project-list";
 import { getBlogPosts } from "@/lib/blog";
+import Image from "next/image";
 
 export default function Page() {
   const posts = getBlogPosts()
     .sort(
       (a, b) =>
         new Date(b.metadata.date).getTime() -
-        new Date(a.metadata.date).getTime()
+        new Date(a.metadata.date).getTime(),
     )
     .slice(0, 3);
 
   return (
     <section className="space-y-8">
+      <div className="flex items-center gap-4">
+        <Image
+          className="rounded-full"
+          src="/pfp.jpeg"
+          width={100}
+          height={100}
+          alt="profile picture"
+        />
+        <div>
+          <h1 className="text-3xl font-bold">Dastan Ozgeldi</h1>
+          <p className="text-muted-foreground">
+            18 / software engineer @ defect.kz
+          </p>
+        </div>
+      </div>
+
       <div>
         <div className="flex items-center justify-between">
           <h3 className="text-lg tracking-tighter font-mono">about me</h3>
@@ -23,12 +40,14 @@ export default function Page() {
             see resume
           </Link>
         </div>
-        <p className="mt-2">
-          I&apos;m an 18 y.o. developer from Almaty, Kazakhstan. I create
-          websites, mobile apps, discord/telegram bots and many other stuff. I
-          love watching Whiplash, redesigning this page every other week and
-          abusing LLMs to win hackathons.
-        </p>
+        <div>
+          <p className="mt-2">
+            I&apos;m an 18 y.o. developer from Almaty, Kazakhstan. I create
+            websites, mobile apps, discord/telegram bots and many other stuff. I
+            love watching Whiplash, redesigning this page every other week and
+            abusing LLMs to win hackathons.
+          </p>
+        </div>
       </div>
 
       <div>
