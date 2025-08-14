e5102e6c78f6ec79d42db1774d207cc30a7aa6ff
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-08-15 03:15:08 +0500

    feat: open project links in a new tab

--- src/components/project-list.tsx
+++ src/components/project-list.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { ExternalLinkIcon } from "lucide-react";
 import Image from "next/image";
 import { site } from "@/config/site";
 import data from "@/config/projects.json";
@@ -10,27 +10,33 @@ export function ProjectList({ extended = false }: { extended?: boolean }) {
   return (
     <ul className="space-y-4">
       {projects.map((project) => {
-        const LinkComponent = project.href.startsWith("http") ? "a" : Link;
         return (
           <li key={project.name} className="border rounded-lg p-3">
-            <LinkComponent
-              href={project.href}
+            <a
               className="flex flex-col space-y-1.5 mb-3"
+              href={project.href}
+              target="_blank"
+              rel="noopener noreferrer"
             >
-              <div>
-                <span className="font-medium underline underline-offset-4">
-                  {project.name}
-                </span>
-                {project.tag && (
-                  <Badge className="ml-2 font-mono font-normal">
-                    {project.tag}
-                  </Badge>
-                )}
+              <div className="flex items-center justify-between">
+                <div>
+                  <span className="font-medium underline underline-offset-4 ">
+                    {project.name}
+                  </span>
+
+                  {project.tag && (
+                    <Badge className="ml-2 font-mono font-normal">
+                      {project.tag}
+                    </Badge>
+                  )}
+                </div>
+                <ExternalLinkIcon size={16} />
               </div>
+
               <span className="text-muted-foreground">
                 {project.description}
               </span>
-            </LinkComponent>
+            </a>
 
             {extended && (
               <Image
