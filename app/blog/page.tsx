b5316935e74ecd34be3da6baa90b1ecb875bddb6
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-11-04 22:45:45 +0900

    improve blog page design

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -14,20 +14,15 @@ export default async function Page() {
   const posts = await getPosts();
 
   return (
-    <>
+    <div className="flex-1">
       <h1 className="text-2xl font-medium tracking-tighter mb-6">blog</h1>
-      <div className="flex flex-col gap-8">
+      <div className="flex flex-col gap-4">
         {posts.map((post) => (
           <Link key={post.slug} href={`/blog/${post.slug}`} className="group">
-            <div className="flex w-full flex-col gap-y-1">
-              <p className="text-lg font-medium group-hover:underline group-hover:decoration-neutral-400 group-hover:underline-offset-4">
+            <div className="flex sm:items-center sm:justify-between w-full flex-col sm:flex-row gap-y-1">
+              <span className="text-lg font-medium group-hover:underline group-hover:decoration-neutral-400 group-hover:underline-offset-4">
                 {post.title.toLowerCase()}
-              </p>
-              {post.description && (
-                <p className="prose prose-neutral">
-                  {post.description.toLowerCase()}
-                </p>
-              )}
+              </span>
               <div className="text-sm text-muted-foreground">
                 {formatDate(post.date, {
                   short: true,
@@ -41,7 +36,7 @@ export default async function Page() {
           </Link>
         ))}
       </div>
-    </>
+    </div>
   );
 }
 
