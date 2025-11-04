93d445d37f2f6088ab5f7b1892c7e97678aefe7d
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-11-04 21:39:12 +0900

    refactor: unused prop distinction

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -10,7 +10,7 @@ export default function Page() {
   return (
     <>
       <h1 className="text-2xl font-medium tracking-tighter mb-6">projects</h1>
-      <ProjectList extended />
+      <ProjectList />
     </>
   );
 }
