08c96660ef96dfe24a49a95d079fb7172d5485d7
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-11-04 21:32:55 +0900

    fix: bg color outside body tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,10 +64,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className="bg-linear-to-b from-background to-blue-50">
       <body
         className={cn(
-          "py-3 px-6 max-w-2xl m-auto min-h-screen flex flex-col space-y-6 bg-gradient-to-b from-background to-blue-50",
+          "py-3 px-6 max-w-2xl m-auto min-h-screen flex flex-col space-y-6",
           inter.className
         )}
       >
