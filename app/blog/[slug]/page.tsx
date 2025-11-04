2f271b5da0567a3003b40e85563708294a277309
Dastan Ozgeldi <ozgdastan@gmail.com>
2025-11-04 23:08:25 +0900

    make sure to generate metadata

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,4 +1,36 @@
+import { Metadata } from "next";
 import PostHeader from "./_components/post-header";
+import { getPost } from "@/lib/blog";
+import { formatDate } from "@/lib/formatters";
+import { site } from "@/config/site";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Promise<Metadata | undefined> {
+  const post = await getPost(params.slug);
+  if (!post) {
+    return;
+  }
+
+  const publishedTime = formatDate(post.date);
+
+  return {
+    title: post.title,
+    openGraph: {
+      title: post.title,
+      publishedTime,
+      type: "article",
+      url: `${site.url}/blog/${post.slug}`,
+    },
+    twitter: {
+      title: post.title,
+      card: "summary_large_image",
+      creator: "@dastanozgeldi",
+    },
+  };
+}
 
 export default async function Post({
   params,
